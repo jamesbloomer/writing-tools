6a744824f7655bacee382c3b07b18697378084ab
James <github2@thebloomers.co.uk>
2015-01-24 11:33:47 +0000

    Add word count tracker and publish script

---
+++ publish.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+git add -A
+./scripts/words-changed.sh
+vim log.txt
+git add -A
+git commit -m "$1"
+git push origin master
---
+++ words-changed.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+ADDED=$(git diff --staged --word-diff=porcelain text/ | grep -P '^\+(?!\+)' | wc -w)
+REMOVED=$(git diff --staged --word-diff=porcelain text/ | grep -P '^-(?!-)' | wc -w)
+DATE=$(date "+%F %T %z")
+TIMESTAMP=$(date +%s)
+echo $DATE,$ADDED words added, $REMOVED words removed
+echo $TIMESTAMP,$DATE,$ADDED,$REMOVED >> stats.txt
